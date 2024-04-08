eb76e1ba1e08555708482d87133d2e64d9ee7494
fraz haider <faraz323257@gmail.com>
2024-04-08 02:31:08 -0700

    update header component with user data

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
     },
     profilePicture: {
       type: String,
-      default: "https://www.bing.com/images/search?view=detailV2&ccid=tmgWGdzG&id=73FCD0D09D76D2AED5BEE4F9DED8614488FE5E9C&thid=OIP.tmgWGdzGFmIwg1iaqCbSvgHaHa&mediaurl=https%3a%2f%2fwisdomexperience.org%2fwp-content%2fuploads%2f2019%2f10%2fblank-profile-picture-973460_960_720.png&exph=720&expw=720&q=Blank+Profile&simid=608009985046415874&FORM=IRPRST&ck=C45DB1FF933574B2852CB702A76BF04C&selectedIndex=12&itb=0",
+      default: "https://th.bing.com/th?q=Blank+Profile&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.3&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
     },
   },
   { timestamps: true }

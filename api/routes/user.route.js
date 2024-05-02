1bfbba8e33a74bad2161a29eb387b7b72fd8c66b
fraz haider <faraz323257@gmail.com>
2024-05-02 07:48:37 -0700

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { test, updateUser } from "../controlers/user.controler.js";
+import { deleteUser, test, updateUser } from "../controlers/user.controler.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 router.get("/test",test);
 router.put("/update/:userId" ,verifyToken, updateUser);
+router.delete("/delete/:userId",verifyToken,deleteUser);
 export default router;

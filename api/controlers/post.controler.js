e03254dccef9f4c2338679269bb2a5491528bb5f
fraz haider <faraz323257@gmail.com>
2024-05-13 13:43:30 -0700

    add delete post functionality to the dashboard

--- api/controlers/post.controler.js
+++ api/controlers/post.controler.js
@@ -38,8 +38,8 @@ export const getposts = async (req, res, next) => {
       ...(req.query.postId && { _id: req.query.postId }),
       ...(req.query.searchTerm && {
         $or: [
-          { title: { $regex: req.query.searchTerm, $options: 'i'} },
-          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
         ],
       }),
     })
@@ -47,23 +47,35 @@ export const getposts = async (req, res, next) => {
       .skip(startIndex)
       .limit(limit);
 
-      const totalPosts = await Post.countDocuments();
-      const now = new Date();
-      const oneMonthAgo = new Date(
-        now.getFullYear(),
-        now.getMonth() -1,
-        now.getDate(),
-      );
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
 
-      const lastMonthPosts = await Post.countDocuments({
-        createdAt:{$gte:oneMonthAgo},
-      });
-      res.status(200).json({
-        posts,
-        totalPosts,
-        lastMonthPosts,
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-      })
+export const deletepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403,'You are not alowed to delete this post'))
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json('The post has been deleted');
+    
   } catch (error) {
     next(error);
   }

3140e2def06de019ee3774b360463e47340549f3
fraz haider <faraz323257@gmail.com>
2024-06-02 23:57:59 -0700

    show the comments of a post

--- api/controlers/comment.controler.js
+++ api/controlers/comment.controler.js
@@ -1,6 +1,5 @@
 import Comment from "../models/comment.model.js";
 
-
 export const createComment = async (req, res, next) => {
   try {
     const { content, postId, userId } = req.body;
@@ -16,6 +15,17 @@ export const createComment = async (req, res, next) => {
     });
     await newComment.save();
     res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { deleteUser, getUsers, signout, test, updateUser } from "../controlers/user.controler.js";
+import { deleteUser, getUser, getUsers, signout, test, updateUser } from "../controlers/user.controler.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 router.get("/test",test);
@@ -8,5 +8,6 @@ router.put("/update/:userId" ,verifyToken, updateUser);
 router.delete("/delete/:userId",verifyToken,deleteUser);
 router.post("/signout",signout)
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId',getUser);
 
 export default router;
